d110c1640e4212f6c6456dddc1abe180190776dd
testingapitest0-lab <testingapitest0@gmail.com>
2026-08-01 12:00:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ ACCOUNTS = {
         "warehouse_id": "600040"
     },
 
-    "Nelamangala": {
+    "Nelamangala WH": {
         "cred": "sanjay.mahto@swissmilitaryindia.com",
         "password": "Emiza@123",
         "seller_id": "80000476",
